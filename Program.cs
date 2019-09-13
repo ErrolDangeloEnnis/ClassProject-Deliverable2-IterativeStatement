2a4f291ea3bd3e7c4f09d24b2890c75bc3d8890b
Errol Ennis <erroldangelo@usf.edu>
2019-09-12 23:33:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassProject_Deliverable2_IterativeStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * Author:Errol Ennis
+             * Date:September 12, 2019
+             * Description: Asks students to enter desired grade and displays grade letter.
+             */
+
+            Console.WriteLine("Enter a number between 1-100");
+
+            try
+            {
+                string input = Console.ReadLine();
+                int value = int.Parse(input);
+
+                if((value > 0) && (value < 101))
+                {
+                    while (value > 0)
+                    {
+                        Console.WriteLine("You have entered " + value.ToString());
+                        Console.WriteLine("This is the current integer value in the loop: " + value.ToString());
+                        value--;
+                    }//end of while
+                }//end of if
+
+                else
+                {
+                    Console.WriteLine("Try entering a number between 1-100");
+                }//end of else
+                
+            }//end of try
+
+            catch
+            {
+                Console.WriteLine("Enter a valid integer");
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey(true);
+            }//end of catch
+        }
+    }
+}
